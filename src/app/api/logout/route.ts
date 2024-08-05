5c840fa3cceaa9ca67a65d393714071b42c379da
Krysto <quy.p.hoang@gmail.com>
2024-08-05 22:40:53 +0700

    delete session when logging out

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,6 +1,15 @@
 import { cookies } from 'next/headers';
+import { getAccount } from '@/lib/auth';
+import db from '@/lib/db';
 
 export async function POST() {
+  const account = await getAccount();
+  if (account) {
+    await db
+      .deleteFrom('sessions')
+      .where('id', '=', account?.sessionId)
+      .execute();
+  }
   cookies().delete('auth_token');
   return Response.json({ ok: true });
 }
