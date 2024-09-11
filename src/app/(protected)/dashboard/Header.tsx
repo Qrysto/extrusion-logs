7a2819541c2efdd0c95fa1ebbba941ab5e7b0c6e
Krysto <quy.p.hoang@gmail.com>
2024-09-11 21:54:47 +0700

    employee ID input

--- src/app/(protected)/dashboard/Header.tsx
+++ src/app/(protected)/dashboard/Header.tsx
@@ -4,12 +4,15 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Check } from 'lucide-react';
 import { LoggedInAccount } from '@/lib/auth';
 import AccountControl from './AccountControl';
 import AddExtrusionLog from './AddExtrusionLog';
 
 export default function Header({ account }: { account: LoggedInAccount }) {
   const [employeeId, setEmployeeId] = useState('');
+  const [editing, setEditing] = useState(false);
 
   return (
     <header className="bg-background border-b py-6 flex justify-between items-start shrink-0">
@@ -30,13 +33,46 @@ export default function Header({ account }: { account: LoggedInAccount }) {
       </Link>
 
       <div className="flex justify-end items-center flex-1">
-        <Input
-          value={employeeId}
-          name="employee_id"
-          onChange={(evt) => setEmployeeId(evt.target.value)}
-          placeholder="Employee ID"
-          className="w-40"
-        />
+        {!editing && !!employeeId && (
+          <label
+            onClick={() => {
+              setEditing(true);
+            }}
+            className="cursor-pointer"
+          >
+            Employee ID: <strong>{employeeId}</strong>
+          </label>
+        )}
+        {!editing && (
+          <Button
+            variant="link"
+            onClick={() => {
+              setEditing(true);
+            }}
+          >
+            {employeeId ? 'Change' : 'Enter Employee ID'}
+          </Button>
+        )}
+        {editing && (
+          <form
+            className="flex items-center"
+            onSubmit={() => {
+              setEditing(false);
+            }}
+          >
+            <Input
+              autoFocus
+              value={employeeId}
+              name="employee_id"
+              onChange={(evt) => setEmployeeId(evt.target.value)}
+              placeholder="Employee ID"
+              className="w-40"
+            />
+            <Button type="submit" className="ml-2">
+              <Check className="h-4 w-4" />
+            </Button>
+          </form>
+        )}
         <AccountControl account={account} />
       </div>
     </header>
