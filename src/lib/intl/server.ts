db4c4d0ae664f85f07e2771252ae556b77033cfa
Krysto <quy.p.hoang@gmail.com>
2024-11-08 13:41:01 +0700

    set default locale to vi

--- src/lib/intl/server.ts
+++ src/lib/intl/server.ts
@@ -1,7 +1,7 @@
 import { translateEn, translateVi, translateKr } from './translate';
 
 export const locales = ['en', 'vi', 'kr'];
-export const defaultLocale = 'en';
+export const defaultLocale = 'vi';
 
 export function getTranslate(locale: string) {
   const translate =
